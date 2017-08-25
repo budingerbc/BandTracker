e3940f4add01adefdf2c7e84e4cffcf4e406c3d9
Ben Budinger <budingerbc@gmail.com>
2017-08-25 16:10:02 -0700

    Complete front-end with basic CRUD functionality for Venue class with dynamic adding of bands to venues.

--- BandTracker/Controllers/HomeController.cs
+++ BandTracker/Controllers/HomeController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using BandTracker.Models;
 using System.Collections.Generic;
 using System;
+using System.Linq;
 
 namespace BandTracker.Controllers
 {
@@ -31,14 +32,83 @@ namespace BandTracker.Controllers
       Venue newVenue = new Venue(Request.Form["venue-name"]);
       newVenue.Save();
 
-      return View("Venues", Venue.GetAll());
+      return View("Index", Venue.GetAll());
     }
 
     [HttpGet("/venue-details/{id}")]
     public ActionResult VenueDetails(int id)
     {
-      return View(Venue.Find(id));
+      Dictionary<string, object> model = new Dictionary<string, object> {};
+
+      Venue foundVenue = Venue.Find(id);
+      List<Band> allBands = Band.GetAll();
+      List<Band> venueBands = foundVenue.GetBandHistory();
+
+      List<Band> uniqueBands = allBands.Except(venueBands).ToList();
+
+      model.Add("venue", foundVenue);
+      model.Add("bands", uniqueBands);
+
+      return View(model);
     }
+
+    [HttpGet("/venue-details/{id}/edit")]
+    public ActionResult EditVenue(int id)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object> {};
+
+      Venue editVenue = Venue.Find(id);
+
+      return View(editVenue);
+    }
+
+    [HttpPost("/venue-details/{id}/updated")]
+    public ActionResult VenueDetailsEdited(int id)
+    {
+      Venue updateVenue = Venue.Find(id);
+      updateVenue.Update(Request.Form["venue-name"]);
+
+      Dictionary<string, object> model = new Dictionary<string, object> {};
+
+      Venue foundVenue = Venue.Find(id);
+      List<Band> allBands = Band.GetAll();
+      List<Band> venueBands = foundVenue.GetBandHistory();
+
+      List<Band> uniqueBands = allBands.Except(venueBands).ToList();
+
+      model.Add("venue", foundVenue);
+      model.Add("bands", uniqueBands);
+
+      return View("VenueDetails", model);
+    }
+
+
+    [HttpPost("/venue-details/{id}/bands-added")]
+    public ActionResult VenueDetailsBandsAdded(int id)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object> {};
+
+      Venue updatedVenue = Venue.Find(id);
+
+      string bandValues = Request.Form["bands"];
+      string[] split = bandValues.Split(',');
+
+      foreach(var bandId in split)
+      {
+        updatedVenue.AddBand(Band.Find(int.Parse(bandId)));
+      }
+
+      List<Band> allBands = Band.GetAll();
+      List<Band> venueBands = updatedVenue.GetBandHistory();
+
+      List<Band> unqiueBands = allBands.Except(venueBands).ToList();
+
+      model.Add("venue", updatedVenue);
+      model.Add("bands", unqiueBands);
+
+      return View("VenueDetails", model);
+    }
+
 
     [HttpGet("/bands")]
     public ActionResult Bands()
@@ -59,6 +129,60 @@ namespace BandTracker.Controllers
       newBand.Save();
 
       return View("Bands", Band.GetAll());
+    }
+
+    [HttpGet("/band-details/{id}")]
+    public ActionResult BandDetails(int id)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object> {};
+
+      Band foundBand = Band.Find(id);
+      List<Venue> allVenues = Venue.GetAll();
+      List<Venue> venuesPlayed = foundBand.GetVenuesPlayedAt();
+
+      List<Venue> uniqueVenues = allVenues.Except(venuesPlayed).ToList();
+
+      model.Add("band", foundBand);
+      model.Add("venues", uniqueVenues);
+      model.Add("venuesPlayed", venuesPlayed);
+
+      return View(model);
+    }
+
+    [HttpPost("/band-details/{id}/venues-added")]
+    public ActionResult BandDetailsVenuesAdded(int id)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object> {};
+
+      Band updatedBand = Band.Find(id);
+
+      string venuesValues = Request.Form["venues"];
+      string[] split = venuesValues.Split(',');
+
+      foreach(var venueId in split)
+      {
+        updatedBand.AddVenue(Venue.Find(int.Parse(venueId)));
+      }
+
+      List<Venue> allVenues = Venue.GetAll();
+      List<Venue> venuesPlayed = updatedBand.GetVenuesPlayedAt();
+
+      List<Venue> uniqueVenues = allVenues.Except(venuesPlayed).ToList();
+
+      model.Add("band", updatedBand);
+      model.Add("venues", uniqueVenues);
+      model.Add("venuesPlayed", venuesPlayed);
+
+      return View("BandDetails", model);
+    }
+
+    [HttpPost("/venue-deleted/{id}")]
+    public ActionResult VenueDeleted(int id)
+    {
+      Venue deleteMe = Venue.Find(id);
+      deleteMe.Delete();
+
+      return View("Index", Venue.GetAll());
     }
   }
 }
